728c144b03cc534db5de2acb24468258df7c70d6
Thisun-17 <thisun.20233173@iit.ac.lk>
2025-06-17 23:41:46 +0530

    Add kanbanBoard function with initial task data

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function kanbanBoard() {
+    return {
+        // This is our data - like variables that Alpine.js can use
+        tasks: [
+            { id: 1, title: 'First Task' },
+            { id: 2, title: 'Second Task' },
+            { id: 3, title: 'Third Task' }
+        ]
+    }
+}
